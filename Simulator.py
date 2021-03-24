c43aa89b5680410558e51944672a6d5eff5e130f
jayanthbellam <69341097+jayanthbellam@users.noreply.github.com>
2021-03-24 22:38:59 +0530

    Update Simulator.py

--- Simulator.py
+++ Simulator.py
@@ -7,22 +7,26 @@ Created on Wed Mar 24 21:50:33 2021
 
 Registers=[0 for j in range(32)]
 
-def readfile(K):
+def readFile(K):
     #should return a line every time it is caled 
     return K.readline()
 
-def Fetch(inst):
+def fetch(inst):
     Registers[0],temp=[int(x) for x in inst.split()]
     return temp
 
-def Decode():
+def decode():
     #should return opcode,fun3,func6/7,rs1,rs2,rd,imm
     pass
 
-def Execute():
+def execute():
     pass
 def memoryAcess():
     pass
 
-def WriteBack():
+def writeBack():
+    pass
+
+def setToDefault():
+    #this should make sure that after completion/before start of program registers and memory is set to its original state
     pass
