bffd96289259fb75f5f45f162ccc80bc57ed2b62
jayanthbellam <69341097+jayanthbellam@users.noreply.github.com>
2021-03-31 09:08:38 +0530

    Updated Execute()

--- Simulator.py
+++ Simulator.py
@@ -40,9 +40,60 @@ def fetch(inst):
 def decode():
     #should update the CONTROL_SIGNALS
     pass
-
-def execute():
-    pass
+def execute(arg1,arg2,ALU_control):
+    '''
+    0: signed addition
+    1: bitwise AND
+    2: bitwise OR
+    3: SLL
+    4: SLT
+    5: SRA
+    6: SRl
+    7: SUB
+    8: XOR
+    9: MUL
+    10: DIV
+    11: REM
+    12: unsigned addition
+    '''
+    if ALU_control==0:
+        return arg1+arg2
+    if ALU_control==1:
+        return arg1&arg2
+    if ALU_control==2:
+        return arg1|arg2
+    if ALU_control==3:
+        temp=bin(arg1).replace('0b','')
+        temp=temp[arg2:]
+        temp=int(temp,base=2)
+        return temp
+    if ALU_control==4:
+        return 1 if arg1<arg2 else 0
+        pass
+    if ALU_control==5:
+        temp=bin(arg1).replace('0b', '')
+        temp=temp[:-arg2]
+        temp='1'*arg2 +temp
+        return int(temp)
+    if ALU_control==6:
+        temp=bin(arg1).replace('0b', '')
+        temp=temp[:-arg2]
+        temp='0'*arg2 +temp
+        return int(temp)
+    if ALU_control==7:
+        return arg1-arg2
+    if ALU_control==8:
+        return arg1^arg2
+    if ALU_control==9:
+        return arg1*arg2
+    if ALU_control==10:
+       if arg2==0: #convetion
+           return bintodec('1'*32)
+       return arg1//arg2
+    if ALU_control==11:
+        return arg1%arg2
+    if ALU_control==12:
+        return bin(int(arg1)+int(arg2)).replace('0b','')
 
 def memoryAcess():
     pass
