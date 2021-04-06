953bcc9b004c8599b0e7da72d1f79e479dd14b9b
jayanthbellam <69341097+jayanthbellam@users.noreply.github.com>
2021-04-06 16:55:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Mar 24 20:48:31 2021
-
-@author: jayanth
-"""
 import sys
 import Simulator
 
 if len(sys.argv)<2:
     print("Error")
 else:
-    pass
+    Simulator.readFile(sys.argv[1])
+    for i in range(4):
+        Simulator.run_RISCVsim()
