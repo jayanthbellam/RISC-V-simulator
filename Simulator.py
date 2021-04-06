d85bf39ff9639e3333abf4d59d65dc7da7f84130
Sai-Teja-Sj <81324223+Sai-Teja-Sj@users.noreply.github.com>
2021-04-06 13:00:36 +0530

    Update Simulator.py

--- Simulator.py
+++ Simulator.py
@@ -176,17 +176,18 @@ def memoryAcess(PC,ALUout,opcode):
     else:
        return -1 
 
-def writeback(data,rm,PC):   #data from memory ,# rm  from excute for ALU instructions ,# rd destination register 
+def writeback(data,rm,PC):  #data from memory ,# rm  from excute for ALU instructions ,# rd destination register 
+    rd1=int(rd,base=2)
     if(operation =="add" or operation=="slt" or operation=="and" or operation =="or" or operation=="sll" or operation=="sra" or operation=="mul"):
-        Reg[rd]=rm
+        Reg[rd1]=rm
     elif(operation== "srl" or operation=="sub" or operation=="xor" or operation=="div" or operation=="rem" or operation=="addi" or operation=="andi" or operation=="ori"):
-        Reg[rd]=rm
+        Reg[rd1]=rm
     elif(operation=="lb" or operation=="ld" or operation=="lh" or operation=="lw"):
-        Reg[rd]=data
+        Reg[rd1]=data
     elif(operation=="jalr" or operation=="jal"):
-        Reg[rd]=PC
+        Reg[rd1]=PC
     elif(operation=="lui" or operation=="auipc"):
-        Reg[rd]=rm
+        Reg[rd1]=rm
         
 def setToStart():
     #restores the registers to the original state
