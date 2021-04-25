f532d3464072832766da022540cd58ac2dc2aa6b
jayanthbellam <69341097+jayanthbellam@users.noreply.github.com>
2021-04-25 18:46:47 +0530

    Update hazard.py

--- Phase2/hazard.py
+++ Phase2/hazard.py
@@ -1,40 +1,135 @@
-from Instruction import State
-class HDU:
+from Instruction import State,ControlUnit,BranchTargetBuffer
+class HAZ:
     def __init__(self):
-       
         self.E2E=0
-        self.M2E=0
         self.M2M=0
+        self.M2E=0
         self.E2D=0
         self.M2D=0
-        
-    def check_data_hazard(self,states):
-        forwarding_paths = set()
-       
-        new_states = []     # updated states
-        new_states = [states[0]]
-        toDecode = states[1]
-        toExecute = states[2]
-        toMem = states[3]
-        toWB = states[4]
-        isHazard = False    # is there a data hazard?
-        doStall = False     # do we need to stall in case of data forwarding?
-        stallWhere = 3      # stall at the decode stage or execute stage?
-                            # 1 = at execute, 2 = at decode, 3 = don't stall
-                            # Sorted according to priority
+    
+    def data_hazard(self,state_):
+        new_states=[]
+        data_forwards= set()
+        de=state_[1]
+        ex=state_[2]
+        me=state_[3]
+        wb=state_[4]
+        Hazard =0     #  0/1
+        stall=0    #0 / 1
+        w_stall=3
+        de_opcode=de.opcode
+        ex_opcode=ex.opcode
+        me_opcode=me.opcode
+        wb_opcode=wb.opcode
+	 
+	    #m2m       3 for load 35 is store
+        if(wb_opcode==3 and me_opcode==35) and (wb.rd>0 and wb.rd==me.rs2):
+                Hazard=1
+                me.RB=wb.MDR
+                self.M2M=wb.MDR
+                data_forwards.add("M2M")
+	
+	    #m2e     load 
+        if wb.rd >0:
+            if wb.rd==ex.rs1:
+                ex.RA=wb.MDR
+                Hazard=1
+                self.M2E=wb.MDR
+                data_forwards.add("M2E")
+            
+            if wb.rd==ex.rs2:
+                ex.RB=wb.MDR
+                Hazard=1
+                self.M2E=wb.MDR
+                data_forwards.add("M2E")
 
-        toDecode_opcode = toDecode.IR & (0x7F)
-        toExecute_opcode = toExecute.IR & (0x7F)
-        toMem_opcode = toMem.IR & (0x7F)
-        toWB_opcode = toWB.IR & (0x7F)
-        
-       
+		 
+ 	        #e2e
+            if me.rd>0:
+                if me_opcode==3:
+                    if ex_opcode==35:
+                        if ex.rs1==me.rd:
+                            Hazard=1
+                            stall=1
+                            w_stall=min(w_stall,1)
+                    else:
+                        Hazard=1
+                        stall=1
+                        w_stall=min(w_stall,1)
+			 
+                else:
+                    if (ex.rs1==me.rd):
+                        ex.RA=me.ALU_out
+                        self.E2E=me.ALU_out
+                        Hazard=1
+                        data_forwards.add("E2E")
 
-        # M->M forwarding
-       
-        if (toWB_opcode==3) and toMem_opcode==35:
-            if toWB.rd > 0 and toWB.rd == toMem.rs2:
-                toMem.RB = toWB.RY
-                isHazard = True
-                self.M2M=toWB.RY
-                forwarding_paths.add("M->M")
+                    if (ex.rs2==me.rd):
+                        ex.RB=me.ALU_out
+                        self.E2E=me.ALU_out
+                        Hazard=1
+                        data_forwards.add("E2E")
+            
+            if (de_opcode==99 or de_opcode==103):
+                #M2D
+                if wb.rd>0:
+                    if wb.rd==de.rs1:
+                        de.rs1=wb.MDR
+                        self.M2D=wb.MDR
+                        Hazard=1
+                        data_forwards.add("M2D")
+                    if wb.rd==de.rs2:
+                        de.rs2=wb.MDR
+                        Hazard=1
+                        self.M2D=wb.MDR
+                        data_forwards.add("M2D")
+	        #E2D
+                if me.rd>0 :
+                    if me_opcode ==3:
+                        Hazard=1
+                        stall=1
+                        w_stall=min(w_stall,2)	
+                    else:
+                        if me.rd==de.rs1:
+                            de.rs1=me.ALU_out
+                            self.E2D=me.ALU_out
+                            Hazard=1
+                            data_forwards.add("E2D")
+			 
+                        if me.rd ==de.rs2 :
+                            de.rs2=me.ALU_out
+                            self.E2D=me.ALU_out
+                            Hazard=1
+                            data_forwards.add("E2D")
+	
+                if ex.rd > 0 and (ex.rd== de.rs1 or ex.rd ==de.rs2):
+                    Hazard=1
+                    stall=1
+                    w_stall=min(w_stall,2)
+	    		   
+        new_states= new_states + [de,ex,me,wb]
+        return [Hazard,stall,new_states,w_stall,data_forwards]
+
+    def check_data_haz_stall(self,states):
+        de=states[1]
+        fe=states[0]
+        ex=states[2]
+        me=states[3]
+        if (ex.rd!=-1 and de.rs1!=-1) and (ex.is_actual_instruction!=False and de.is_actaul_instruction!=False): 
+            if ex.rd == de.rs1 :
+                if ex.rd!=0 :
+                    return [True,2]
+                
+            if ex.rd==de.rs2:
+                if ex.rd!=0:
+                    return [True,2]
+	            
+        if (me.rd!=-1 and de.rs1!=-1) and (me.is_actual_instruction!=False and de.is_actual_instruction!=False):
+            if me.rd == de.rs1:
+                if me.rd!=0 :
+                    return [True,1]
+            
+            if me.rd==de.rs2:
+                if me.rd!=0:
+                    return [True,1]	
+        return [False,-1]
