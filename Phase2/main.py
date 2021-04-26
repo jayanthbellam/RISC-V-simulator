ce5d02498fda1182dc79b40aac4c74893acd07da
jayanthbellam <69341097+jayanthbellam@users.noreply.github.com>
2021-04-26 18:00:55 +0530

    Update main.py

--- Phase2/main.py
+++ Phase2/main.py
@@ -1,6 +1,105 @@
 from Instruction import State,ControlUnit,BranchTargetBuffer
-from Hazard import HAZ
 import sys
+
+def data_hazard(state_):
+    new_states=[state_[0]]
+    data_forwards= 0
+    de=state_[1]
+    ex=state_[2]
+    me=state_[3]
+    wb=state_[4]
+    Hazard =False
+    stall=False    #0 / 1
+    w_stall=3
+    de_opcode=de.opcode
+    ex_opcode=ex.opcode
+    me_opcode=me.opcode
+    wb_opcode=wb.opcode
+    if(wb_opcode==3 and me_opcode==35) and (wb.rd>0 and wb.rd==me.rs2):
+            Hazard=True
+            me.RB=wb.MDR
+            data_forwards+=1
+    if wb.rd >0:
+        if wb.rd==ex.rs1:
+            ex.RA=wb.MDR
+            Hazard=True
+            data_forwards+=1
+        
+        if wb.rd==ex.rs2:
+            ex.RB=wb.MDR
+            Hazard=True
+            data_forwards+=1
+    if me.rd>0:
+        if me_opcode==3:
+            if ex_opcode==35:
+                if ex.rs1==me.rd:
+                    Hazard=True
+                    stall=True
+                    w_stall=min(w_stall,1)
+            else:
+                Hazard=True
+                stall=True
+                w_stall=min(w_stall,1)
+
+        else:
+            if (ex.rs1==me.rd):
+                ex.RA=me.Alu_out
+                Hazard=True
+                data_forwards=True
+            if (ex.rs2==me.rd):
+                ex.RB=me.Alu_out
+                Hazard=True
+                data_forwards+=1
+        
+    if (de_opcode==99 or de_opcode==103):
+        if wb.rd>0:
+            if wb.rd==de.rs1:
+                de.branchRA=wb.MDR
+                Hazard=True
+                data_forwards+=1
+            if wb.rd==de.rs2:
+                de.branchRB=wb.MDR
+                Hazard=True
+                data_forwards+=1
+        if me.rd>0 :
+            if me_opcode ==3:
+                Hazard=True
+                stall=True
+                w_stall=min(w_stall,2)	
+            else:
+                if me.rd==de.rs1:
+                    de.branchRA=me.Alu_out
+                    Hazard=True
+                    data_forwards+=1
+                if me.rd ==de.rs2 :
+                    de.branchRB=me.Alu_out
+                    Hazard=True
+                    data_forwards+=1
+        if ex.rd > 0 and (ex.rd== de.rs1 or ex.rd ==de.rs2):
+            Hazard=True
+            stall=True
+            w_stall=min(w_stall,2)
+ 		   
+    new_states= new_states + [de,ex,me,wb]
+    return [Hazard,stall,new_states,w_stall,data_forwards]
+def check_data_haz_stall(states):
+    de=states[2]
+    ex=states[3]
+    me=states[4]
+    if 	ex.is_actual_instruction and de.is_actual_instruction:
+        if ex.rd>0:
+            print(de.rs1,de.rs2)
+            if ex.rd==de.rs1:
+                return [True,2]
+            if ex.rd==de.rs2:
+                return [True,2]
+    if me.is_actual_instruction and de.is_actual_instruction:
+        if me.rd>0:
+            if me.rd==de.rs1:
+                return [True,1]
+            if me.rd==de.rs2:
+                return [True,1]
+    return [False,-1]
 
 if len(sys.argv)<2:
     print("Invalid Number of arguments")
@@ -9,10 +108,9 @@ else:
     ComputerState=ControlUnit(filename)
     PC=0
     clock=0
-    pipelined_execution=True
-    data_forwarding=True
+    pipelined_execution=False
+    data_forwarding=False
     btb=BranchTargetBuffer()
-    hazards=HAZ()
     if pipelined_execution:
         in_states=[State() for i in range(5)]
         out_states=[]
@@ -25,7 +123,7 @@ else:
                 for i in range(4,-1,-1):
                     if i==4:
                         overflow=ComputerState.write_back(in_states[4])
-                        hazard=hazards.data_hazard(in_states)
+                        hazard=data_hazard(in_states)
                         in_states[3]=hazard[2][3]
                         data_hazard_count+=hazard[4]
                     if i==3:
@@ -39,7 +137,7 @@ else:
                         control_change,control_change_pc,tempstate=ComputerState.fetch(in_states[0],btb)
                         out_states.append(tempstate)
                     if i!=4:
-                        hazard=hazards.data_hazard(in_states)
+                        hazard=data_hazard(in_states)
                         in_states=hazard[2]
                         is_hazard=is_hazard|hazard[0]
                         stall=stall|hazard[1]
@@ -70,7 +168,7 @@ else:
         #if data is not forwarded we implement stalling
         else:
             while 1:
-                data_hazards=hazards.check_data_haz_stall(in_states)
+                data_hazards=check_data_haz_stall(in_states)
                 reversed_states=[(idx,val) for idx,val in enumerate(in_states)][::-1]
                 for idx,state in reversed_states:
                     if idx==4:
