92008ed824e777ef16d217d5d8d2422aae986c57
jayanthbellam <69341097+jayanthbellam@users.noreply.github.com>
2021-04-26 18:54:21 +0530

    Update main.py

--- Phase2/main.py
+++ Phase2/main.py
@@ -1,5 +1,102 @@
-from Instruction import State,ControlUnit,BranchTargetBuffer
+from Instruction import ISB,ControlUnit,BranchTargetBuffer
 import sys
+
+def data_hazard(state_):
+    new_states=[state_[0]]
+    data_forwards= 0
+    de=state_[1]
+    ex=state_[2]
+    me=state_[3]
+    wb=state_[4]
+    Hazard =False
+    stall=False    #0 / 1
+    w_stall=3
+    de_opcode=de.opcode
+    ex_opcode=ex.opcode
+    me_opcode=me.opcode
+    wb_opcode=wb.opcode
+    if(wb_opcode==3 and me_opcode==35) and (wb.rd>0 and wb.rd==me.rs2):
+            Hazard=True
+            me.RB=wb.MDR
+            data_forwards+=1
+    if wb.rd >0:
+        if wb.rd==ex.rs1:
+            ex.RA=wb.MDR
+            Hazard=True
+            data_forwards+=1
+        
+        if wb.rd==ex.rs2:
+            ex.RB=wb.MDR
+            Hazard=True
+            data_forwards+=1
+    if me.rd>0:
+        if me_opcode==3:
+            if ex_opcode==35:
+                if ex.rs1==me.rd:
+                    Hazard=True
+                    stall=True
+                    w_stall=min(w_stall,1)
+            else:
+                Hazard=True
+                stall=True
+                w_stall=min(w_stall,1)
+
+        else:
+            if (ex.rs1==me.rd):
+                ex.RA=me.Alu_out
+                Hazard=True
+                data_forwards=True
+            if (ex.rs2==me.rd):
+                ex.RB=me.Alu_out
+                Hazard=True
+                data_forwards+=1
+        
+    if (de_opcode==99 or de_opcode==103):
+        if wb.rd>0:
+            if wb.rd==de.rs1:
+                de.branchRA=wb.MDR
+                Hazard=True
+                data_forwards+=1
+            if wb.rd==de.rs2:
+                de.branchRB=wb.MDR
+                Hazard=True
+                data_forwards+=1
+        if me.rd>0 :
+            if me_opcode ==3:
+                Hazard=True
+                stall=True
+                w_stall=min(w_stall,2)	
+            else:
+                if me.rd==de.rs1:
+                    de.branchRA=me.Alu_out
+                    Hazard=True
+                    data_forwards+=1
+                if me.rd ==de.rs2 :
+                    de.branchRB=me.Alu_out
+                    Hazard=True
+                    data_forwards+=1
+        if ex.rd > 0 and (ex.rd== de.rs1 or ex.rd ==de.rs2):
+            Hazard=True
+            stall=True
+            w_stall=min(w_stall,2)
+ 		   
+    new_states= new_states + [de,ex,me,wb]
+    return [Hazard,stall,new_states,w_stall,data_forwards]
+def check_data_haz_stall(states):
+    de=states[2]
+    ex=states[3]
+    me=states[4]
+    if 	ex.is_actual_instruction and de.is_actual_instruction and ex.rd>0:
+        if ex.rd==de.rs1:
+            return [True,2]
+        if ex.rd==de.rs2:
+            return [True,2]
+    if me.is_actual_instruction and de.is_actual_instruction and me.rd>0:
+        if me.rd==de.rs1:
+            return [True,1]
+        if me.rd==de.rs2:
+            return [True,1]
+    return [False,-1]
 
 if len(sys.argv)<2:
     print("Invalid Number of arguments")
@@ -8,31 +105,24 @@ else:
     ComputerState=ControlUnit(filename)
     PC=0
     clock=0
-    knob1 = int(input("Enable(1)/Disable(0) pipelining: "))
-    knob3 = int(input("Enable(1)/Disable(0) printing the values in the register file at the end of each cycle: "))
-    knob4 = int(input("Enable(1)/Disable(0) printing information in the pipeline registers at the end of each cycle, along with cycle number. "))
-    knob5 = int(input(" Enter the instruction number for printing its pipeline register information/[Disable(-1)]: "))
-    
-    btb = BranchTargetBuffer()
-    hazards = HAZ()
-    if knob1:   #pipelining
-        in_states=[State() for i in range(5)]
+    pipelined_execution=True
+    data_forwarding=True
+    btb=BranchTargetBuffer()
+    if pipelined_execution:
+        in_states=[ISB() for i in range(5)]
         out_states=[]
         data_hazard_count=0
-        #If data forwarding is disabled then pipeline is executed with stalling
-        knob2 = int(input("Enable(1)/Disable(0) data forwarding: "))
-        if knob2: #data forwarding 
+        if data_forwarding:
             while 1:
                 is_hazard=False
                 stall=False
-                w_stall= 3 #3=> no stalling
+                w_stall=3 #3=> no stalling
                 for i in range(4,-1,-1):
                     if i==4:
                         overflow=ComputerState.write_back(in_states[4])
-                        hazard=hazards.data_hazard(in_states)
+                        hazard=data_hazard(in_states)
                         in_states[3]=hazard[2][3]
                         data_hazard_count+=hazard[4]
-                        
                     if i==3:
                         out_states.append(ComputerState.memory_access(in_states[3]))
                     if i==2:
@@ -40,12 +130,11 @@ else:
                     if i==1:
                         control_hazard,control_hazard_pc,tempstate=ComputerState.decode(in_states[1],btb)
                         out_states.append(tempstate)
-                      
                     if i==0:
                         control_change,control_change_pc,tempstate=ComputerState.fetch(in_states[0],btb)
                         out_states.append(tempstate)
                     if i!=4:
-                        hazard=hazards.data_hazard(in_states)
+                        hazard=data_hazard(in_states)
                         in_states=hazard[2]
                         is_hazard=is_hazard|hazard[0]
                         stall=stall|hazard[1]
@@ -59,25 +148,24 @@ else:
                     PC=control_change_pc
                 if control_hazard and stall==False:
                     PC=control_hazard_pc
-                    out_states[0]=State(0)
+                    out_states[0]=ISB(0)
                 if stall:
                     print('\n\nSTALLING\n\n')
                     if w_stall==1:
-                        out_states=[in_states[1],in_states[2],State(),out_states[3]]
+                        out_states=[in_states[1],in_states[2],ISB(),out_states[3]]
                     else:
-                        out_states=[in_states[1],State(),out_states[2],out_states[3]]
+                        out_states=[in_states[1],ISB(),out_states[2],out_states[3]]
                 if not (out_states[0].is_actual_instruction or out_states[1].is_actual_instruction or out_states[2].is_actual_instruction or out_states[3].is_actual_instruction):
                         break
-                in_states=[State(PC)]+out_states
+                in_states=[ISB(PC)]+out_states
                 out_states=[]
                 ComputerState.store_State('pipelined_data_forwarding.txt')
                 input()
-            
-                
+              
         #if data is not forwarded we implement stalling
         else:
             while 1:
-                data_hazards=hazards.check_data_haz_stall(in_states)
+                data_hazards=check_data_haz_stall(in_states)
                 reversed_states=[(idx,val) for idx,val in enumerate(in_states)][::-1]
                 for idx,state in reversed_states:
                     if idx==4:
@@ -99,14 +187,17 @@ else:
                     PC=new_pc
                 if control_hazard and not data_hazards[0]:
                     PC=control_hazard_pc
-                    print(PC)
-                    out_states[0]=State()
+                    out_states[0]=ISB()
                 if data_hazards[0]:
-                    print('\n\ntrue\n\n')
-                    out_states=[in_states[1],State()]+out_states[2:]
+                    if data_hazards[1]==2:
+                        in_states=[in_states[0],in_states[1],in_states[2],ISB(),out_states[3]]
+                        continue
+                    else:
+                        in_states=[in_states[0],in_states[1],ISB(),out_states[2],out_states[3]]
+                        continue
                 if not (out_states[0].is_actual_instruction or out_states[1].is_actual_instruction or out_states[2].is_actual_instruction or out_states[3].is_actual_instruction):
                     break
-                in_states=[State(PC)]+out_states
+                in_states=[ISB(PC)]+out_states
                 out_states=[]
                 ComputerState.store_State('pipelined_not_data_forwarding.txt')
                 input()
@@ -114,9 +205,9 @@ else:
 
 
             
-    #non-pipelined
+
     else:
-        Simulator=State(0)
+        Simulator=ISB(0)
         while 1:
             Simulator=ComputerState.fetch(Simulator,0)
             if not Simulator.is_actual_instruction:
